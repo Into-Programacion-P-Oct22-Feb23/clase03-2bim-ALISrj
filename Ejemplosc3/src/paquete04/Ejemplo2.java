9f953f679b1042cc9755c764153df6f575a4d2df
Alex Ramirez <alexis.rj2110@gmail.com>
2022-12-21 10:45:54 -0500

    Subida3

--- Ejemplosc3/src/paquete04/Ejemplo2.java
+++ Ejemplosc3/src/paquete04/Ejemplo2.java
@@ -16,13 +16,35 @@ public class Ejemplo2 {
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
-        double[][] dato1 = {{1, 2, 3},{6, 8, 9}}; //
+
+        double[][] dato1 = {{1, 2, 3}, {6, 8, 9}}; //
         /*
             1   4   3
             36  64  9
-        */
-        
+         */
+
+        double[][] dato2 = new double[2][3];
+        int fila;
+        int col;
+
+        for (fila = 0; fila < dato1.length; fila++) {
+            for (col = 0; col < dato1[fila].length; col++) {
+                if (dato1[fila][col] % 2 == 0) {
+                    dato2[fila][col] = dato1[fila][col] * dato1[fila][col];
+                } else {
+                    dato2[fila][col] = dato1[fila][col];
+                }
+
+            }
+
+        }
+        for (fila = 0; fila < dato1.length; fila++) {
+            for (col = 0; col < dato1[fila].length; col++) {
+                System.out.printf("%.0f\t", dato2[fila][col]); // POR: ALEX RAMIREZ
+            }
+            System.out.println();
+        }
+
     }
-    
+
 }
